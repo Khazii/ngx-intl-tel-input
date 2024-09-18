c6182b95b898730a3dc2af0ba7cc14a4ad8854ee
Ollie <oliverbrennan05@gmail.com>
2024-09-18 14:04:24 +0100

    asdf

--- projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.component.spec.ts
+++ projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NgxIntlTelInputComponent } from './ngx-intl-tel-input.component';
 
@@ -6,7 +6,7 @@ describe('NgxIntlTelInputComponent', () => {
   let component: NgxIntlTelInputComponent;
   let fixture: ComponentFixture<NgxIntlTelInputComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [NgxIntlTelInputComponent],
     }).compileComponents();
--- projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.validator.ts
+++ projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.validator.ts
@@ -10,60 +10,69 @@ https://github.com/angular/angular/issues/18025
 https://stackoverflow.com/a/54075119/1617590
 */
 export const phoneNumberValidator = (control: any) => {
-	if (!control.value) {
-		return;
-	}
-	// Find <input> inside injected nativeElement and get its "id".
-	const el: HTMLElement = control.nativeElement as HTMLElement;
-	const inputBox: HTMLInputElement | any = el
-		? el.querySelector('input[type="tel"]')
-		: undefined;
-	if (inputBox) {
-		const id = inputBox.id;
-		const isCheckValidation = inputBox.getAttribute('validation');
-		if (isCheckValidation === 'true') {
-			const isRequired = control.errors && control.errors.required === true;
-			const error = { validatePhoneNumber: { valid: false } };
+  if (!control.value) {
+    return;
+  }
+  // Find <input> inside injected nativeElement and get its "id".
+  const el: HTMLElement = control.nativeElement as HTMLElement;
+  const inputBox: HTMLInputElement | any = el
+    ? el.querySelector('input[type="tel"]')
+    : undefined;
+  if (inputBox) {
+    const isCheckValidation = inputBox.getAttribute('validation');
+    if (isCheckValidation === 'true') {
+      const isRequired = control.errors && control.errors.required === true;
+      const error = { validatePhoneNumber: { valid: false } };
 
-			inputBox.setCustomValidity('Invalid field.');
+      inputBox.setCustomValidity('Invalid field.');
 
-			let number: lpn.PhoneNumber;
+      let number: lpn.PhoneNumber | null = null;
 
-			try {
-				number = lpn.PhoneNumberUtil.getInstance().parse(
-					control.value.number,
-					control.value.countryCode
-				);
-			} catch (e) {
-				if (isRequired) {
-					return error;
-				} else {
-					inputBox.setCustomValidity('');
-				}
-			}
+      try {
+        number = lpn.PhoneNumberUtil.getInstance().parse(
+          control.value.number,
+          control.value.countryCode,
+        );
+      } catch (e) {
+        if (isRequired) {
+          return error;
+        } else {
+          inputBox.setCustomValidity('');
+        }
+      }
 
-			if (control.value) {
-				// @ts-ignore
-        if (!number) {
-					return error;
-				} else {
-					if (
-						!lpn.PhoneNumberUtil.getInstance().isValidNumberForRegion(
-							number,
-							control.value.countryCode
-						)
-					) {
-						return error;
-					} else {
-						inputBox.setCustomValidity('');
-					}
-				}
-			}
-		} else if (isCheckValidation === 'false') {
-			inputBox.setCustomValidity('');
+      if (control.value) {
+        // Check if the number is valid for the region
+        if (
+          !number ||
+          !lpn.PhoneNumberUtil.getInstance().isValidNumberForRegion(
+            number,
+            control.value.countryCode,
+          )
+        ) {
+          return error;
+        }
 
-			control.clearValidators();
-		}
-	}
-	return;
+        // Check for specific prefixes "07624" or "+447624"
+        const formattedNumber = lpn.PhoneNumberUtil.getInstance().format(
+          number,
+          lpn.PhoneNumberFormat.E164,
+        );
+        if (
+          !formattedNumber.startsWith('+447624') &&
+          !formattedNumber.startsWith('07624')
+        ) {
+          inputBox.setCustomValidity('Invalid number prefix.');
+          return error;
+        }
+
+        inputBox.setCustomValidity('');
+      }
+    } else if (isCheckValidation === 'false') {
+      inputBox.setCustomValidity('');
+
+      control.clearValidators();
+    }
+  }
+  return;
 };
